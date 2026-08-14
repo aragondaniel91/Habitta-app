b64f3af6e5b7c04857c3f0033562afc6685a424f
aragondaniel91 <79597132+aragondaniel91@users.noreply.github.com>
2026-08-14 17:54:24 -0500

    HAB-171 mount assemblies routes

--- apps/api/src/operations-routes.ts
+++ apps/api/src/operations-routes.ts
@@ -1,6 +1,8 @@
+import { assembliesRoutes } from './assemblies-routes';
 import { operationsRoutes as baseOperationsRoutes } from './operations-routes-base';
 import { maintenanceFinancialRoutes } from './maintenance-financial-routes';
 
 baseOperationsRoutes.route('/', maintenanceFinancialRoutes);
+baseOperationsRoutes.route('/', assembliesRoutes);
 
 export const operationsRoutes = baseOperationsRoutes;
